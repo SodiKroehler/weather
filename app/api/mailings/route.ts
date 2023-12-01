8d07594f2c62c1a03dc192ade32b5fddd0aef7a3
SodiKroehler <sodikroehler@outlook.com>
2023-12-01 12:35:14 -0500

    dynamic messages

--- app/api/mailings/route.ts
+++ app/api/mailings/route.ts
@@ -1,8 +1,18 @@
 import { NextResponse, NextRequest} from 'next/server'
+import getWeather from './../weatherCall'
 
 export async function GET(req: NextRequest) {
 
-    let returnVal = "test {} (412) 448-2736 | hi brianna this is v {} (260) 312-5235"
+    let names = ["sodi", "brianna"]
+    let numbers = ["(412) 448-2736", "(260) 312-5235"]
+    let ammassed = ""
 
-    return NextResponse.json({messages: returnVal})
+    for (let i=0; i<2; i++){
+        let msg = "Hi " + names[i] + ". " + getWeather()
+        ammassed = ammassed + "|" + msg + "{}" + numbers[i]
+    }
+
+    let returnVal = "test {}  | hi brianna this is v {} (260) 312-5235"
+
+    return NextResponse.json({messages: ammassed})
 }
