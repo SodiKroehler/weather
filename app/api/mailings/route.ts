b08199911cec0471c91d57140b2a314de4915b31
SodiKroehler <sodikroehler@outlook.com>
2023-12-01 12:11:23 -0500

    testing splits

--- app/api/mailings/route.ts
+++ app/api/mailings/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest} from 'next/server'
 
 export async function GET(req: NextRequest) {
 
-    let returnVal = "test, (412) 448-2736"
+    let returnVal = "test {} (412) 448-2736 | weird} junk{ even this }{ {} (412) 448-2736"
 
     return NextResponse.json({messages: returnVal})
 }
